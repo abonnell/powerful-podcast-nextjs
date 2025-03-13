4abe4829f495201419be143ea59e47d55b6ab5df
Andrew Bonnell <abonnell1005@gmail.com>
2025-03-12 22:38:47 -0700

    navbar fix for mui theming

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+"use client";
+import { Link } from "@mui/material";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Box,
+  Button,
+  useTheme,
+} from "@mui/material";
+import { usePathname } from "next/navigation";
+
+// import styles from "./navbar.module.css";
+import { Root, classes } from "./styles";
+import Image from "next/image";
+
+export default function Navbar({ navLinks }) {
+  const navArr = Object.values(navLinks);
+  const path = usePathname();
+  const theme = useTheme();
+
+  return (
+    <Root>
+      <AppBar>
+        <Container>
+          <Toolbar>
+            <Link href={""}>
+              <Image src="/logo.png" alt="logo" width={50} height={50} />
+            </Link>
+            <Box>
+              {navArr.map((item, index) => {
+                return (
+                  <Button
+                    // variant="text"
+                    key={`${item.key}-${index}`}
+                    href={item.path}
+                    className={`${classes.navItem} ${
+                      path === item.path ? classes.currentPage : ""
+                    }`}
+                  >
+                    {item.key.toUpperCase()}
+                  </Button>
+                );
+              })}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Root>
+  );
+}
---
+++ src/components/Navbar/styles.jsx
@@ -0,0 +1,23 @@
+import { styled } from "@mui/material";
+
+const component = "navbar";
+
+const classes = {
+  navItem: `${component}-navItem`,
+  currentPage: `${component}-currentPage`,
+};
+
+const Root = styled("div")(({ theme }) => ({
+  [`& .${classes.navItem}`]: {
+    color: theme.palette.text.primary,
+    padding: "1rem",
+    "&:hover": {
+      color: theme.palette.primary.main,
+    },
+  },
+  [`& .${classes.currentPage}`]: {
+    color: theme.palette.primary.main,
+  },
+}));
+
+export { Root, classes };
