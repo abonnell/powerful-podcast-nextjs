904972f28eead6919ced12b454f4b4cd496134f0
Andrew Bonnell <abonnell1005@gmail.com>
2025-03-17 21:36:44 -0700

    blog comp

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { Box, Typography, useTheme } from "@mui/material";
+import { Root, classes } from "./styles";
+import Link from "next/link";
+
+export default function Blog({ img, imgAlt, title, href, previewText }) {
+  const theme = useTheme();
+  return (
+    <Root>
+      <Box className={classes.container}>
+        <Image src={img} alt={imgAlt} className={classes.art} />
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "6.5%",
+            left: "26%",
+            background:
+              "linear-gradient(to top, rgb(0, 0, 0), rgba(0, 0, 0, 0.5))",
+            height: 100,
+          }}
+        >
+          <Typography className={classes.titleText} variant="h5" component="h2">
+            {title}
+          </Typography>
+
+          <Typography
+            className={classes.previewText}
+            variant="body1"
+            component="p"
+          >
+            {previewText}
+          </Typography>
+        </Box>
+      </Box>
+    </Root>
+  );
+}
---
+++ src/components/Blog/styles.jsx
@@ -0,0 +1,48 @@
+import { styled } from "@mui/material";
+
+const component = "blog";
+
+const classes = {
+  container: `${component}-container`,
+  art: `${component}-art`,
+  textContainer: `${component}-textContainer`,
+  titleText: `${component}-titleText`,
+  previewText: `${component}-previewText`,
+};
+
+const Root = styled("div")(({ theme }) => ({
+  [`& .${classes.container}`]: {
+    padding: "1.5rem 0 1.5rem 0",
+    textAlign: "center",
+    position: "relative",
+    overflow: "hidden",
+    objectFit: "none",
+  },
+  [`& .${classes.art}`]: {
+    objectFit: "cover",
+    width: 400,
+    height: 200,
+  },
+  [`& .${classes.textContainer}`]: {
+    position: "absolute",
+    bottom: "6.5%",
+    left: "26%",
+    background: "linear-gradient(to top, rgb(0, 0, 0), rgb(0, 0, 0))",
+    height: 100,
+  },
+  [`& .${classes.titleText}`]: {
+    color: "white",
+    marginBottom: "0.5rem",
+    width: 400,
+    textAlign: "left",
+    padding: "0 0.5rem 0 0.5rem",
+  },
+  [`& .${classes.previewText}`]: {
+    color: "white",
+    textAlign: "left",
+    width: 400,
+    padding: "0 0.5rem 0 0.5rem",
+  },
+}));
+
+export { Root, classes };
